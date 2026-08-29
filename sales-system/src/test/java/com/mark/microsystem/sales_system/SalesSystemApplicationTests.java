ba22dc2039a89ea7105a15d919cdee123d292e6d
Marcos Magaña <imarcosmagana@gmail.com>
2026-08-29 08:35:18 -0600

    Sales System initial

---
+++ sales-system/src/test/java/com/mark/microsystem/sales_system/SalesSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mark.microsystem.sales_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSystemApplicationTests {
+
+	/*
+	@Test
+	void contextLoads() {
+	}
+	 */
+
+}
