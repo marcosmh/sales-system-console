96e79bfce92943db6d9cf15c951330b6d54e7cb9
Marcos Magaña <imarcosmagana@gmail.com>
2026-08-31 21:36:27 -0600

    new colors were added

--- sales-system/src/main/java/com/mark/microsystem/sales/system/main/utils/ConsoleColors.java
+++ sales-system/src/main/java/com/mark/microsystem/sales/system/main/utils/ConsoleColors.java
@@ -15,7 +15,21 @@ public class ConsoleColors {
     public static final String CYAN = "\u001B[36m";
     public static final String WHITE = "\u001B[37m";
 
+    // Methods for brilliant colors
+    public static final String BRIGHT_BLACK = "\u001B[90m";
+    public static final String BRIGHT_RED = "\u001B[91m";
+    public static final String BRIGHT_GREEN = "\u001B[92m";
+    public static final String BRIGHT_YELLOW = "\u001B[93m";
+    public static final String BRIGHT_BLUE = "\u001B[94m";
+    public static final String BRIGHT_PURPLE = "\u001B[95m";
+    public static final String BRIGHT_CYAN = "\u001B[96m";
+    public static final String BRIGHT_WHITE = "\u001B[97m";
+
     // Methods for wrapping text
+    public static String black(String text) {
+        return BLACK + text + RESET;
+    }
+
     public static String red(String text) {
         return RED + text + RESET;
     }
@@ -42,5 +56,37 @@ public class ConsoleColors {
 
     public static String white(String text) {
         return WHITE + text + RESET;
+    }
+
+    public static String brightBlack(String text) {
+        return BRIGHT_BLACK + text + RESET;
+    }
+
+    public static String brightRed(String text) {
+        return BRIGHT_RED + text + RESET;
+    }
+
+    public static String brightGreen(String text) {
+        return BRIGHT_GREEN + text + RESET;
+    }
+
+    public static String brightYellow(String text) {
+        return BRIGHT_YELLOW + text + RESET;
+    }
+
+    public static String brightBlue(String text) {
+        return BRIGHT_BLUE + text + RESET;
+    }
+
+    public static String brightPurple(String text) {
+        return BRIGHT_PURPLE + text + RESET;
+    }
+
+    public static String brightCyan(String text) {
+        return BRIGHT_CYAN + text + RESET;
+    }
+
+    public static String brightWhite(String text) {
+        return BRIGHT_WHITE + text + RESET;
     }
 }
