74a1e43cb2ca9763398671729b3b6ceb409e5b95
Marcos Magaña <imarcosmagana@gmail.com>
2026-09-10 00:02:25 -0600

    Remove a method that is no longer necessary.

--- sales-system/src/main/java/com/mark/microsystem/sales/system/main/controller/SaleConsole.java
+++ sales-system/src/main/java/com/mark/microsystem/sales/system/main/controller/SaleConsole.java
@@ -51,8 +51,6 @@ public class SaleConsole {
             if (repeat) consoleUtils.pause(textIO);
         }
 
-
-
         if(details.isEmpty()) {
             System.out.println( colors.orange( "\nNo products were added to the sale." ) );
             consoleUtils.pause(textIO);
@@ -63,7 +61,7 @@ public class SaleConsole {
 
         try {
             SaleResponse sale = saleService.createSale(request);
-            printReceipt2(sale, textIO);
+            printReceipt(sale, textIO);
             consoleUtils.pause(textIO);
         } catch(RuntimeException e) {
             System.out.println( colors.red( "\nError creating sale: \n"  + e.getMessage()) +"\n"+ e.getStackTrace() );
@@ -71,29 +69,7 @@ public class SaleConsole {
         }
     }
 
-
-    private void printReceipt(SaleResponse sale) {
-        System.out.println( colors.green( "\nSales Receipt" ) );
-        System.out.println( colors.cyan( String.format( "%-5s %-25s %-20s", "SALE ID", "DATE", "SELLER" )));
-        System.out.println( colors.cyan( "--------------------------------------------------------------------------" ) );
-        System.out.printf(  colors.pinkLight("%-5s %-25s %-20s "),  sale.id(), sale.date(), sale.user().username());
-        System.out.println( colors.cyan( "--------------------------------------------------------------------------" ) );
-
-        for(SaleDetailResponse detail : sale.details()) {
-            ProductSummaryResponse product = detail.product();
-            System.out.printf(  colors.pinkLight(
-                    product.name() + " x" + detail.quantity()
-                            + " = $" + consoleUtils.formatMoney(detail.subtotal())
-            ));
-        }
-
-        System.out.println( colors.cyan( "--------------------------------------------------------------------------" ) );
-
-        System.out.printf(  colors.pinkLight( colors.green("TOTAL: $" + consoleUtils.formatMoney(sale.total())) ));
-
-    }
-
-    private void printReceipt2(SaleResponse sale, TextIO textIO) {
+    private void printReceipt(SaleResponse sale, TextIO textIO) {
         System.out.println( colors.green( "\nSales Receipt " ));
         System.out.println( colors.cyan( "\nSale ID: " ) + colors.pinkLight( String.valueOf(sale.id())) );
         System.out.println( colors.cyan( "\nDate: " ) + colors.pinkLight( String.valueOf(sale.date())) );
